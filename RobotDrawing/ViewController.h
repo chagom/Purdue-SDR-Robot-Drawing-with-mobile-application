e006719fcd4256a22b7f5e60c01e502600b5b0fe
차고음 <phto_kuro@naver.com>
2019-03-13 16:02:01 +0100

    Initial Commit

---
+++ RobotDrawing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RobotDrawing
+//
+//  Created by GoEum Cha on 3/13/19.
+//  Copyright © 2019 GoEum Cha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RobotDrawing/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RobotDrawing
+//
+//  Created by GoEum Cha on 3/13/19.
+//  Copyright © 2019 GoEum Cha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
